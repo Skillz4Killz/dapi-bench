b9b2108c6e7cfdd7d0f7d7c705825c8bcbb07f09
Dan <31395415+cakedan@users.noreply.github.com>
2021-06-04 20:22:38 -0600

    Update detritus.js
    i think itll work

--- memory/detritus.js
+++ memory/detritus.js
@@ -3,12 +3,11 @@ const { GatewayIntents } = require("detritus-client-socket/lib/constants");
 const { TOKEN, OWNER_ID } = require("../configs-node");
 const { READY, SHARD_READY, logMemory } = require("../utils/events-node");
 
-const detritusStarted = Date.now();
-let detritusTime = Date.now();
-let detritusCounter = 0;
+
+const cache = {counter: 0, started: Date.now(), time: Date.now()};
 
 const commandClient = new CommandClient(TOKEN, {
-  prefix: "asdgwq43ewfae3fwawev",
+  prefix: '!',
   gateway: {
     intents: [
       GatewayIntents.DIRECT_MESSAGE_REACTIONS,
@@ -25,53 +24,68 @@ const commandClient = new CommandClient(TOKEN, {
   },
 });
 
-commandClient.on("ready", () => {
-  READY(detritusStarted);
+
+let started = false;
+commandClient.add({
+  name: 'starttests',
+  onBefore: (context) => context.userId === OWNER_ID,
+  run: (context) => {
+    logCacheSizes(context.client);
+    if (!started) {
+      started = true;
+      setInterval(() => {
+        logCacheSizes(context.client);
+      }, 60000);
+    }
+  },
 });
 
+
 (async () => {
-  const detritus = await commandClient.run();
-  detritus.on("ready", () => {
-    READY(detritusStarted);
+  const cluster = await commandClient.run();
+  cluster.on('ready', () => {
+    READY(cache.started);
   });
 
   let shardsLoaded = 0;
-
-  detritus.on("messageCreate", (message) => {
-    if (message.message.author.id !== OWNER_ID) return;
-    if (message.message.content !== "!starttests") return;
-
-    detritusCounter = logMemory(
-      process.memoryUsage(),
-      detritusCounter,
-      "detritus",
-      0,
-      0,
-      0,
-      0
-    );
-    setInterval(() => {
-      detritusCounter = logMemory(
-        process.memoryUsage(),
-        detritusCounter,
-        "detritus",
-        0,
-        0,
-        0,
-        0
-      );
-    }, 60000);
-  });
-
-  detritus.on("gatewayReady", (id) => {
+  cluster.on('gatewayReady', (event) => {
     shardsLoaded++;
-    detritusTime = SHARD_READY(id.raw.shard[0], detritusTime);
 
-    if (shardsLoaded === 10) {
-      READY(detritusStarted);
+    cache.time = SHARD_READY(event.shard.shardId, cache.time);
+    if (shardsLoaded === cluster.shards.length) {
+      READY(cache.started);
     }
   });
 
-  // detritus has received the READY payload, do stuff now
-  console.log(`Client has loaded with a shard count of ${detritus.shardCount}`);
+  console.log(`Client has loaded with a shard count of ${cache.shardCount}`);
 })();
+
+
+function logCacheSizes(shardClient) {
+  const counts = {channels: 0, guilds: 0, members: 0, messages: 0, users: 0};
+  if (shardClient.cluster) {
+    for (let [shardId, shard] of shard.cluster) {
+      addCacheSizes(shard, counts);
+    }
+  } else {
+    addCacheSizes(shardClient, counts);
+  }
+  return cache.counter = logMemory(
+    process.memoryUsage(),
+    cache.counter,
+    'detritus',
+    counts.channels,
+    counts.guilds,
+    counts.members,
+    counts.messages,
+  );
+}
+
+function addCacheSizes(shard, counts) {
+  counts.channels += shard.channels.length;
+  counts.guilds += shard.guilds.length;
+  counts.members += shard.members.length;
+  counts.messages += shard.messages.length;
+  counts.roles += shard.roles.length;
+  counts.users += shard.users.length;
+}
