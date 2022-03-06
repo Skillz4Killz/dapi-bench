e0e0f22a85922b55575007c92014c74f378007b8
Tim Smart <tim.smart@arisechurch.com>
2022-03-07 11:41:17 +1300

    Add optimized version of droff benchmark

--- memory/optimized/droff.js
+++ memory/optimized/droff.js
@@ -1,8 +1,8 @@
-const { createClient, Intents } = require("droff");
+const { createClient, Intents, CacheTTLStore } = require("droff");
 const Rx = require("rxjs");
 const RxO = require("rxjs/operators");
-const { TOKEN, OWNER_ID } = require("../configs-node");
-const { READY, SHARD_READY, logMemory } = require("../utils/events-node");
+const { TOKEN, OWNER_ID } = require("../../configs-node");
+const { READY, SHARD_READY, logMemory } = require("../../utils/events-node");
 
 const droffStarted = Date.now();
 let droffTime = Date.now();
@@ -11,26 +11,42 @@ let droffCounter = 0;
 const droff = createClient({
   token: TOKEN,
   gateway: {
-    intents:
-      Intents.DIRECT_MESSAGE_REACTIONS |
-      Intents.DIRECT_MESSAGES |
-      Intents.GUILD_BANS |
-      Intents.GUILD_EMOJIS_AND_STICKERS |
-      Intents.GUILD_INVITES |
-      Intents.GUILD_MEMBERS |
-      Intents.GUILD_MESSAGE_REACTIONS |
-      Intents.GUILD_MESSAGES |
-      Intents.GUILD_VOICE_STATES |
-      Intents.GUILDS,
+    intents: Intents.DIRECT_MESSAGES | Intents.GUILD_MEMBERS | Intents.GUILD_MESSAGES | Intents.GUILDS,
   },
 });
 
 // Load caches
-const [guildsCache, guildsCache$] = droff.guildsCache();
-const [membersCache, membersCache$] = droff.membersCache();
-const [dmsCache, dmsCache$] = droff.directMessagesCache();
-const [messagesCache, messagesCache$] = droff.messagesCache();
-const [channelsCache, channelsCache$] = droff.channelsCache();
+const second = 1000;
+const minute = 60 * second;
+
+const [guildsCache, guildsCache$] = droff.guildsCache(
+  CacheTTLStore.createNonParent({
+    ttl: 60 * minute,
+  })
+);
+const [membersCache, membersCache$] = droff.membersCache(
+  CacheTTLStore.create({
+    ttl: 30 * minute,
+    strategy: "activity",
+  })
+);
+const [dmsCache, dmsCache$] = droff.directMessagesCache(
+  CacheTTLStore.createNonParent({
+    ttl: 10 * minute,
+    strategy: "activity",
+  })
+);
+const [messagesCache, messagesCache$] = droff.messagesCache(
+  CacheTTLStore.create({
+    ttl: 10 * minute,
+    strategy: "activity",
+  })
+);
+const [channelsCache, channelsCache$] = droff.channelsCache(
+  CacheTTLStore.create({
+    ttl: 61 * minute,
+  })
+);
 
 // Start client and caches
 Rx.merge(droff.effects$, guildsCache$, dmsCache$, messagesCache$, membersCache$, channelsCache$).subscribe();
@@ -59,7 +75,7 @@ Rx.firstValueFrom(starttests$).then(() => {
       "droff",
       await guildsCache.size(),
       await membersCache.size(),
-      (await messagesCache.size()) + (await dmsCache.size()),
+      (await dmsCache.size()) + (await messagesCache.size()),
       await channelsCache.size()
     );
   };
